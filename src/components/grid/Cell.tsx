dd1d9befa96d7d1b8af2b48a98dc151fb13edf70
jvalal <jvalal@gmail.com>
2022-02-11 16:02:43 -0800

    Update Cell.tsx

--- src/components/grid/Cell.tsx
+++ src/components/grid/Cell.tsx
@@ -22,7 +22,7 @@ export const Cell = ({
   const animationDelay = `${position * REVEAL_TIME_MS}ms`
 
   const classes = classnames(
-    'w-10 h-10 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded dark:text-white',
+    'w-12 h-12 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded dark:text-white',
     {
       'bg-white dark:bg-slate-900 border-slate-200 dark:border-slate-600':
         !status,
