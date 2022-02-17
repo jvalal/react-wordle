5155a55ebc851af12e8fe8ef869b4637c45dedba
jvalal <jvalal@gmail.com>
2022-02-17 14:13:56 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -286,14 +286,6 @@ function App() {
         {ABOUT_GAME_MESSAGE}
       </button>
 
-      <button
-        type="button"
-        className="mx-auto mt-5 flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 select-none"
-        onClick={() => setIsAboutModalOpen(true)}
-      >
-        {ABOUT_GAME_MESSAGE}
-      </button>
-
       <Alert message={NOT_ENOUGH_LETTERS_MESSAGE} isOpen={isNotEnoughLetters} />
       <Alert
         message={WORD_NOT_FOUND_MESSAGE}
