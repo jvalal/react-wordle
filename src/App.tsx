ddba10e77a51ae9522976337888243ad158297c8
jvalal <jvalal@gmail.com>
2022-03-20 18:40:35 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -211,7 +211,7 @@ function App() {
     <div className="pt-2 pb-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div className="flex w-100 mx-auto items-center mb-0 mt-0">
       <InformationCircleIcon
-          className="justify-start h-6 w-6 mr-0 cursor-pointer dark:stroke-white"
+          className="justify-start h-6 w-6 mr-20 cursor-pointer dark:stroke-white"
           onClick={() => setIsInfoModalOpen(true)}
         />
         <h1 className="text-2xl text-center grow font-bold dark:text-white">
