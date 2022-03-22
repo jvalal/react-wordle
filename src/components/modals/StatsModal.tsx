fe7297b709798eef33a8fae6d39c9f98c902a253
jvalal <jvalal@gmail.com>
2022-03-21 20:19:54 -0700

    Update StatsModal.tsx

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -55,16 +55,7 @@ export const StatsModal = ({
       </h4>
       
       <Histogram gameStats={gameStats} />
-      {(isGameLost || isGameWon) && (
-        <div className="mt-5 sm:mt-6 columns-2 dark:text-white">
-          <div>
-            <h5>{NEW_WORD_TEXT}</h5>
-            <Countdown
-              className="text-lg font-medium text-gray-900 dark:text-gray-100"
-              date={tomorrow}
-              daysInHours={true}
-            />
-            <button
+      <button
             type="button"
             className="mt-2 w-full rounded-md border border-transparent px-4 py-2 bg-sky-600 text-base font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:text-sm"
             onClick={() => {
@@ -74,6 +65,15 @@ export const StatsModal = ({
           >
             {MORE_TEXT}
           </button>
+      {(isGameLost || isGameWon) && (
+        <div className="mt-5 sm:mt-6 columns-2 dark:text-white">
+          <div>
+            <h5>{NEW_WORD_TEXT}</h5>
+            <Countdown
+              className="text-lg font-medium text-gray-900 dark:text-gray-100"
+              date={tomorrow}
+              daysInHours={true}
+            />
           </div>
         </div>
         
