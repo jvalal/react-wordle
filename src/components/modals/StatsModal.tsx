16cabd66a50094e9c143a6b4ee75e5c9284abab5
jvalal <jvalal@gmail.com>
2022-03-21 20:09:23 -0700

    Update StatsModal.tsx

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -53,6 +53,16 @@ export const StatsModal = ({
       <h4 className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-100">
         {GUESS_DISTRIBUTION_TEXT}
       </h4>
+      <button
+            type="button"
+            className="mt-2 w-full rounded-md border border-transparent px-4 py-2 bg-sky-600 text-base font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:text-sm"
+            onClick={() => {
+              shareStatus(guesses, isGameLost, isHardMode)
+              handleShare()
+            }}
+          >
+            {MORE_TEXT}
+          </button>
       <Histogram gameStats={gameStats} />
       {(isGameLost || isGameWon) && (
         <div className="mt-5 sm:mt-6 columns-2 dark:text-white">
@@ -65,18 +75,6 @@ export const StatsModal = ({
             />
           </div>
           <StatBar gameStats={gameStats} />
-          <button
-            type="button"
-            className="mt-2 w-full rounded-md border border-transparent px-4 py-2 bg-sky-600 text-base font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:text-sm"
-            onClick={() => {
-              shareStatus(guesses, isGameLost, isHardMode)
-              handleShare()
-            }}
-          >
-            {MORE_TEXT}
-          </button>
-
-          
         </div>
         
       )}
