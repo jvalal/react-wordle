3027f961635c9c3942902f13b7926c1d86b7ad60
jvalal <jvalal@gmail.com>
2022-03-22 21:47:25 -0700

    Update StatsModal.tsx

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -61,11 +61,11 @@ export const StatsModal = ({
       <button
             type="button"
             className="mt-2 w-full rounded-md border border-transparent px-2 py-2 bg-sky-600 text-xl font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:text-xl"
-            <td onClick={()=> window.open("http://github.com", "_blank")}>text</td>
-            {/* onClick={() => {
+            
+            onClick={() => {
               shareStatus(guesses, isGameLost, isHardMode)
               handleShare()
-            }} */}
+            }}
           >
             {SHARE_TEXT}
           </button>
