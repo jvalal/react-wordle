c98571f175edc0b9a1f2a17b30706befa0b5c435
jvalal <jvalal@gmail.com>
2022-03-22 23:14:40 -0700

    Update StatsModal.tsx

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -60,7 +60,7 @@ export const StatsModal = ({
       
       <button
             type="button"
-            className="mt-2 w-full rounded-md border border-transparent px-2 py-2 bg-sky-600 text-xl font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:text-xl"
+            className="mt-2 w-1/2 rounded-md border border-transparent px-2 py-2 bg-sky-600 text-xl font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:text-xl"
 
             onClick={() => {
               shareStatus(guesses, isGameLost, isHardMode)
