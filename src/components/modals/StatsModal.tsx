235ead35709a87a0ec68e3ac6c371517641329ae
jvalal <jvalal@gmail.com>
2022-03-23 18:29:04 -0700

    Update StatsModal.tsx

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -79,6 +79,18 @@ export const StatsModal = ({
               daysInHours={true}
             />
             
+              <p className="text-sm text-gray-500 dark:text-gray-300">
+        This is just another Wordle game with ~12000 words instead of 2500. The icons at the top are easy/hard setting, light/dark, info and your stats. -{' '}
+        <a
+          href="https://github.com/cwackerfuss/react-wordle"
+          className="underline font-bold"
+        >
+          
+        </a>{' '}
+      </p>
+
+
+            
           </div>
         </div>
         
