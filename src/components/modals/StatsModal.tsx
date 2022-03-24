527f14dac8e72c0b1ac72aac581ac46fda890dea
jvalal <jvalal@gmail.com>
2022-03-23 19:31:34 -0700

    Update StatsModal.tsx

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -79,6 +79,8 @@ export const StatsModal = ({
               daysInHours={true}
             />
             
+            <Link to={{ pathname: "https://example.zendesk.com/hc/en-us/articles/123456789-Privacy-Policies" }} target="_blank" />
+
             
           </div>
         </div>
