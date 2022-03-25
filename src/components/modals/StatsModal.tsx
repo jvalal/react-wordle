54e14a105c9b0d98d55d176851a0c5f2775629c6
jvalal <jvalal@gmail.com>
2022-03-25 10:11:40 -0700

    Update StatsModal.tsx

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -1,28 +1,28 @@
-import Countdown from 'react-countdown'
-import { StatBar } from '../stats/StatBar'
-import { Histogram } from '../stats/Histogram'
-import { GameStats } from '../../lib/localStorage'
-import { shareStatus } from '../../lib/share'
-import { tomorrow } from '../../lib/words'
-import { BaseModal } from './BaseModal'
+import Countdown from "react-countdown";
+import { StatBar } from "../stats/StatBar";
+import { Histogram } from "../stats/Histogram";
+import { GameStats } from "../../lib/localStorage";
+import { shareStatus } from "../../lib/share";
+import { tomorrow } from "../../lib/words";
+import { BaseModal } from "./BaseModal";
 import {
   STATISTICS_TITLE,
   GUESS_DISTRIBUTION_TEXT,
   NEW_WORD_TEXT,
   SHARE_TEXT,
   // MORE_TEXT,
-} from '../../constants/strings'
+} from "../../constants/strings";
 
 type Props = {
-  isOpen: boolean
-  handleClose: () => void
-  guesses: string[]
-  gameStats: GameStats
-  isGameLost: boolean
-  isGameWon: boolean
-  handleShare: () => void
-  isHardMode: boolean
-}
+  isOpen: boolean;
+  handleClose: () => void;
+  guesses: string[];
+  gameStats: GameStats;
+  isGameLost: boolean;
+  isGameWon: boolean;
+  handleShare: () => void;
+  isHardMode: boolean;
+};
 
 export const StatsModal = ({
   isOpen,
@@ -43,36 +43,40 @@ export const StatsModal = ({
       >
         <StatBar gameStats={gameStats} />
       </BaseModal>
-    )
+    );
   }
   return (
     <BaseModal
       title={STATISTICS_TITLE}
       isOpen={isOpen}
-      handleClose={handleClose}>
-
+      handleClose={handleClose}
+    >
       <StatBar gameStats={gameStats} />
 
       <h4 className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-100">
         {GUESS_DISTRIBUTION_TEXT}
       </h4>
       <Histogram gameStats={gameStats} />
-      
+
       <button
-            type="button"
-            className="mt-2 w-1/2 rounded-md border border-transparent px-2 py-2 bg-sky-600 text-xl font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:text-xl"
+        type="button"
+        className="mt-2 w-1/2 rounded-md border border-transparent px-2 py-2 bg-sky-600 text-xl font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:text-xl"
+        onClick={() => {
+          shareStatus(guesses, isGameLost, isHardMode);
+          handleShare();
+        }}
+      >
+        {SHARE_TEXT}
+      </button>
 
-            onClick={() => {
-              shareStatus(guesses, isGameLost, isHardMode)
-              handleShare()
-            }}
-          >            
-            {SHARE_TEXT}
-          </button>
-          
+      <Link
+        to={{
+          pathname:
+            "https://example.zendesk.com/hc/en-us/articles/123456789-Privacy-Policies",
+        }}
+        target="_blank"
+      />
 
-          
-          
       {(isGameLost || isGameWon) && (
         <div className="mt-5 sm:mt-6 columns-2 dark:text-white">
           <div>
@@ -82,13 +86,9 @@ export const StatsModal = ({
               date={tomorrow}
               daysInHours={true}
             />
-            
-   
           </div>
         </div>
-        
-        
       )}
     </BaseModal>
-  )
-}
+  );
+};
