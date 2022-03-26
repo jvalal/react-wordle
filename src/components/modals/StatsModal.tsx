336bbd17a0ea046706b6dd6c6960ae8778faab47
jvalal <jvalal@gmail.com>
2022-03-26 13:11:43 -0700

    Update StatsModal.tsx

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -41,7 +41,7 @@ export const StatsModal = ({
     anchor.target = '_blank'
     anchor.click();
   }
-  
+
   if (gameStats.totalGames <= 0) {
     return (
       <BaseModal
@@ -68,7 +68,7 @@ export const StatsModal = ({
 
       <button
         type="button"
-        className="mt-2 w-1/3 rounded-md border border-transparent px-2 py-2 bg-sky-600 text-xl font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:text-xl"
+        className="pr-10 mt-2 w-1/3 rounded-md border border-transparent px-2 py-2 bg-sky-600 text-xl font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:text-xl"
         onClick={() => {
           shareStatus(guesses, isGameLost, isHardMode);
           handleShare();
