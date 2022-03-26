f3e16d3a5155f17a9d4e10bbbdfbd312f76635dd
jvalal <jvalal@gmail.com>
2022-03-26 15:14:09 -0700

    Update StatsModal.tsx

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -1,28 +1,28 @@
-import Countdown from 'react-countdown'
-import { StatBar } from '../stats/StatBar'
-import { Histogram } from '../stats/Histogram'
-import { GameStats } from '../../lib/localStorage'
-import { shareStatus } from '../../lib/share'
-import { tomorrow } from '../../lib/words'
-import { BaseModal } from './BaseModal'
+import Countdown from "react-countdown";
+import { StatBar } from "../stats/StatBar";
+import { Histogram } from "../stats/Histogram";
+import { GameStats } from "../../lib/localStorage";
+import { shareStatus } from "../../lib/share";
+import { tomorrow } from "../../lib/words";
+import { BaseModal } from "./BaseModal";
 import {
   STATISTICS_TITLE,
   GUESS_DISTRIBUTION_TEXT,
   NEW_WORD_TEXT,
   SHARE_TEXT,
   // MORE_TEXT,
-} from '../../constants/strings'
+} from "../../constants/strings";
 
 type Props = {
-  isOpen: boolean
-  handleClose: () => void
-  guesses: string[]
-  gameStats: GameStats
-  isGameLost: boolean
-  isGameWon: boolean
-  handleShare: () => void
-  isHardMode: boolean
-}
+  isOpen: boolean;
+  handleClose: () => void;
+  guesses: string[];
+  gameStats: GameStats;
+  isGameLost: boolean;
+  isGameWon: boolean;
+  handleShare: () => void;
+  isHardMode: boolean;
+};
 
 export const StatsModal = ({
   isOpen,
@@ -34,17 +34,14 @@ export const StatsModal = ({
   handleShare,
   isHardMode,
 }: Props) => {
-<<<<<<< Updated upstream
-=======
 
-  const openInNewTab = () => {
-    const anchor = document.createElement('a');
-    anchor.href = 'https://www.youtube.com/';
-    anchor.target = '_blank'
-    anchor.click();
-  }
+  // const openInNewTab = () => {
+  //   const anchor = document.createElement('a');
+  //   anchor.href = 'https://www.youtube.com/';
+  //   anchor.target = '_blank'
+  //   anchor.click();
+  // }
 
->>>>>>> Stashed changes
   if (gameStats.totalGames <= 0) {
     return (
       <BaseModal
@@ -54,26 +51,22 @@ export const StatsModal = ({
       >
         <StatBar gameStats={gameStats} />
       </BaseModal>
-    )
+    );
   }
   return (
     <BaseModal
       title={STATISTICS_TITLE}
       isOpen={isOpen}
-      handleClose={handleClose}>
-
+      handleClose={handleClose}
+    >
       <StatBar gameStats={gameStats} />
 
       <h4 className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-100">
         {GUESS_DISTRIBUTION_TEXT}
       </h4>
       <Histogram gameStats={gameStats} />
-      
+
       <button
-<<<<<<< Updated upstream
-            type="button"
-            className="mt-2 w-1/2 rounded-md border border-transparent px-2 py-2 bg-sky-600 text-xl font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:text-xl"
-=======
         type="button"
         className=" mr-10 mt-2 w-1/3 rounded-md border border-sky500 px-1 py-1 bg-sky-600 text-l font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-sky-500 sm:text-xl"
         onClick={() => {
@@ -84,7 +77,7 @@ export const StatsModal = ({
         {SHARE_TEXT}
       </button>
 
-      <button
+      {/* <button
               type='button'
               className=" mt-2 w-1/3 rounded-md border border-sky500 px-1 py-1 bg-sky-600 text-l font-medium text-white hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 sm:text-xl"
               onClick={() => {
@@ -92,19 +85,11 @@ export const StatsModal = ({
               }}
             >
              Play More 
-            </button>
+            </button> */}
 
 
 
->>>>>>> Stashed changes
 
-            onClick={() => {
-              shareStatus(guesses, isGameLost, isHardMode)
-              handleShare()
-            }}
-          >
-            {SHARE_TEXT}
-          </button>
       {(isGameLost || isGameWon) && (
         <div className="mt-5 sm:mt-6 columns-2 dark:text-white">
           <div>
@@ -114,12 +99,9 @@ export const StatsModal = ({
               date={tomorrow}
               daysInHours={true}
             />
-            
           </div>
         </div>
-        
-        
       )}
     </BaseModal>
-  )
-}
+  );
+};
